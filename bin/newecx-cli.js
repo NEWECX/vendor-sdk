d0d681500c44c9c7cca6d81c387ffe21b32b71c6
Sam Wen <samw@ritani.com>
2021-08-31 08:48:33 -0400

    improvde color transform

--- bin/newecx-cli.js
+++ bin/newecx-cli.js
@@ -2,9 +2,10 @@
 
 'use strict';
 
-const { get_program, run_program } =  require('../lib/cmds');
+const { get_program, run_program, check_latest_version } =  require('../lib/cmds');
 
 (async () => {
     const program = get_program();
     await run_program(program);
+    check_latest_version(__dirname);
 })();
