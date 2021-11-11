f673b843d6886ab3933f399ee55f497129c07a37
Sam Wen <samw@ritani.com>
2021-11-11 07:49:18 -0500

    add pkg and release to build executibles for macos and windows

--- bin/newecx-cli.js
+++ bin/newecx-cli.js
@@ -2,10 +2,12 @@
 
 'use strict';
 
-const { get_program, run_program, check_latest_version } =  require('../lib/cmds');
+require('../lib/cmds/run-at-top');
+
+const { get_program, run_program, /*check_latest_version*/ } =  require('../lib/cmds');
 
 (async () => {
     const program = get_program();
     await run_program(program);
-    check_latest_version(__dirname);
+    //check_latest_version(__dirname);
 })();
